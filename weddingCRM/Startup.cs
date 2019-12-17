f42d4cb76f78691b9b179c1cfa14a80f1331c559
itsik <itsikh100@gmail.com>
2019-12-17 21:09:27 +0200

    Add project files.

---
+++ weddingCRM/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(weddingCRM.Startup))]
+namespace weddingCRM
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
